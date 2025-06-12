0bcf11de24ba6034094103df0298703e58f9e871
Luyimbazi Oscar <luyimbazioscar53@gmail.com>
2025-06-12 13:12:15 -0700

    added a Quiz-2 game

---
+++ Quiz-2/quiz.js
@@ -0,0 +1,59 @@
+// creating a variable for the user's answer
+const answer1 = prompt(`What is the highest mountain in Africa?:  `)
+ const correctAnswer1 = "KILIMANJARO"
+ let correctAnswers = 0;
+if(answer1.toUpperCase() === correctAnswer1){
+    
+    
+    alert(`You are right`)
+    correctAnswers++;
+    
+}else{
+    alert(`Your answer is wrong`)
+}
+const answer2 = prompt(`How many days are in a week?:  `)
+ const correctAnswer2 = "SEVEN"
+if(answer2.toUpperCase() === correctAnswer2){
+    
+    
+    alert(`You are right`)
+    correctAnswers++;
+}else{
+    alert(`Your answer is wrong`)
+}
+const answer3 = prompt(`What is the capital city of Uganda?:  `)
+ const correctAnswer3 = "KAMPALA"
+if(answer3.toUpperCase() === correctAnswer3){
+    
+    
+    alert(`You are right`)
+    correctAnswers++;
+}else{
+    alert(`Your answer is wrong`)
+}
+const answer4 = prompt(`What is the source of the Nile?:  `)
+ const correctAnswer4 = "LAKE VICTORIA"
+if(answer4.toUpperCase() === correctAnswer4){
+    
+    
+    alert(`You are right`)
+    correctAnswers++;
+}else{
+    alert(`Your answer is wrong`)
+}
+const answer5 = prompt(`What is the best university in Uganda?:  `)
+ const correctAnswer5 = "MAKERERE"
+if(answer5.toUpperCase() === correctAnswer5){
+    
+    
+    alert(`You are right`)
+    correctAnswers++;
+}else{
+    alert(`Your answer is wrong`)
+}
+alert(`You have got ${correctAnswers} answers right`);
+let totalanswers = 5;
+let percent = (correctAnswers/totalanswers) * 100;
+alert(`You have got ${percent}%`);
+
+
